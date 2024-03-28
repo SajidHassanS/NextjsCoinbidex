cb5bb48534ec14f230665e3b60300a5b1a8970de
Sajid Hassan <sajidhassancs@gmail.com>
2024-03-28 12:06:02 +0500

    menu

--- components/Layout/Header.js
+++ components/Layout/Header.js
@@ -118,7 +118,7 @@ const Header = () => {
                   setActiveLink("trade");
                 }}
                 className={
-                  "px-4 py-2 mx-2 cursor-pointer animation-hover flex-col items-start relative flex  w-100" +
+                  "px-4 py-2 mx-2 cursor-pointer animation-hover flex-col items-start relative flex " +
                   (activeLink === "trade"
                     ? " text-blue-800 animation-active "
                     : " text-black-500 hover:text-blue-800 a")
@@ -140,20 +140,20 @@ const Header = () => {
                 </div>
 
                 {isTradeHovered && (
-                  <div className="md:absolute top-8 left-0 right-auto w-full md:w-[300px] bg-white-500  shadow-xl shadow-blue-200 mt-2">
+                  <div className="md:absolute top-8   bg-white-500  shadow-xl shadow-blue-200 mt-2">
                     <div className="p-4 flex justify-between">
                       <div className=" pr-2">
                         {/* Left side menu items here */}
                         <div className="">
-                        <div className="p-3 w-full  md:w-[270px] hover:bg-blue-100">
+                        <div className="p-3   hover:bg-blue-100">
                           <h3 className=" text-black-600">Spot Trading</h3>
                           <p>Buy & Sell on the spot</p>
                           </div>
-                          <div className="p-3 w-full  md:w-[270px] hover:bg-blue-100">
+                          <div className="p-3  md:w-[270px] hover:bg-blue-100">
                           <h3 className=" text-black-600">Spot Trading</h3>
-                          <p>Buy & Sell on the spot</p>
+                          <p>Buy & Sells on the spot</p>
                           </div>
-                          <div className="p-3 w-full  md:w-[270px] hover:bg-blue-100">
+                          <div className="p-3  md:w-[270px] hover:bg-blue-100">
                           <h3 className=" text-black-600">Spot Trading</h3>
                           <p>Buy & Sell on the spot</p>
                           </div>
@@ -277,20 +277,20 @@ const Header = () => {
                 </div>
 
                 {isLearnHovered && (
-                  <div className="md:absolute top-8 left-0 right-auto w-full md:w-[300px] bg-white-500  shadow-xl shadow-blue-200 mt-2">
+                  <div className="md:absolute top-8   bg-white-500  shadow-xl shadow-blue-200 mt-2">
                     <div className="p-4 flex justify-between">
                       <div className=" pr-2">
                         {/* Left side menu items here */}
                         <div className="">
-                        <div className="p-3 w-full  md:w-[270px] hover:bg-blue-100">
+                        <div className="p-3   hover:bg-blue-100">
                           <h3 className=" text-black-600">Spot Trading</h3>
                           <p>Buy & Sell on the spot</p>
                           </div>
-                          <div className="p-3 w-full  md:w-[270px] hover:bg-blue-100">
+                          <div className="p-3  md:w-[270px] hover:bg-blue-100">
                           <h3 className=" text-black-600">Spot Trading</h3>
-                          <p>Buy & Sell on the spot</p>
+                          <p>Buy & Sells on the spot</p>
                           </div>
-                          <div className="p-3 w-full  md:w-[270px] hover:bg-blue-100">
+                          <div className="p-3  md:w-[270px] hover:bg-blue-100">
                           <h3 className=" text-black-600">Spot Trading</h3>
                           <p>Buy & Sell on the spot</p>
                           </div>
